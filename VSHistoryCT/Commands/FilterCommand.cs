ab4de27f896b0a12cf9c38b6693a3521a8039b00
Ken Cross <kjchome@comcast.net>
2025-10-26 11:05:49 -0400

    wip

---
+++ VSHistoryCT/Commands/FilterCommand.cs
@@ -0,0 +1,51 @@
+﻿
+namespace VSHistory;
+
+[Command(PackageIds.VSHistoryFilter)]
+internal sealed class FilterCommand : BaseCommand<FilterCommand>
+{
+    /// <summary>
+    /// The "Open" command is used to open the current VS History
+    /// directory in the file explorer.
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    protected override void Execute(object sender, EventArgs e)
+    {
+        if (g_VSControl?.LatestHistoryFile?.VSHistoryDir == null ||
+            !g_VSControl.LatestHistoryFile.HasHistoryFiles)
+        {
+            return;
+        }
+
+        
+    }
+
+    private static Dictionary<string, int> dicDir = new();
+
+    protected override void BeforeQueryStatus(EventArgs e)
+    {
+        DirectoryInfo? dir = g_VSControl?.LatestHistoryFile?.VSHistoryDir;
+
+        if (dir == null || !g_VSControl.LatestHistoryFile.HasHistoryFiles)
+        {
+            return;
+        }
+
+        int iter;
+        string sPath = dir.FullName;
+        if (dicDir.TryGetValue(sPath, out iter))
+        {
+            dicDir[sPath] = ++iter;
+        }
+        else
+        {
+            iter = 1;
+            dicDir.Add(sPath, iter);
+        }
+
+        Command.Text = $"Iter {iter}";
+
+        base.BeforeQueryStatus(e);
+    }
+}
