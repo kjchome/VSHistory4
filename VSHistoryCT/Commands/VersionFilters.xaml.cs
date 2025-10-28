410e962aa896edae6bd733be67f4cb076f4192b3
Ken Cross <kjchome@comcast.net>
2025-10-28 13:11:35 -0400

    wip

--- VSHistoryCT/Commands/VersionFilters.xaml.cs
+++ VSHistoryCT/Commands/VersionFilters.xaml.cs
@@ -1,11 +1,13 @@
-﻿using System.Collections.ObjectModel;
-using System.ComponentModel;
+﻿using System.ComponentModel;
 using System.Text.Json;
 using System.Windows;
 using System.Windows.Controls;
 
 namespace VSHistory;
 
+/// <summary>
+/// The settings to filter VS History files.
+/// </summary>
 public class FilterSettings : INotifyPropertyChanged
 {
     public bool radExclude1 { get; set; } = false;
@@ -18,16 +20,51 @@ public class FilterSettings : INotifyPropertyChanged
 
     public bool radOrInclude { get; set; } = true;
 
-    public string searchString1 { get; set; } = "Hello";
+    public string searchString1 { get; set; } = "";
 
-    public string searchString2 { get; set; } = "There";
+    public string searchString2 { get; set; } = "";
 
-    public event PropertyChangedEventHandler? PropertyChanged;
+    /// <summary>
+    /// Load the settings from the specified path, if it exists.
+    /// </summary>
+    /// <param name="FilterJsonPath"></param>
+    public FilterSettings(string FilterJsonPath)
+    {
+        try
+        {
+            FileInfo fileInfo = new FileInfo(FilterJsonPath);
+
+            if (fileInfo.Exists)
+            {
+                FilterSettings? filterSettings =
+                    JsonSerializer.Deserialize<FilterSettings>(fileInfo.OpenRead());
+
+                if (filterSettings != null)
+                {
+                    radExclude1 = filterSettings.radExclude1;
+                    radExclude2 = filterSettings.radExclude2;
+                    radInclude1 = filterSettings.radInclude1;
+                    radInclude2 = filterSettings.radInclude2;
+                    searchString1 = filterSettings.searchString1;
+                    searchString2 = filterSettings.searchString2;
+                }
+            }
+        }
+        catch
+        {
+        }
+    }
+
+    public FilterSettings()
+    {
+    }
 
     protected void OnPropertyChanged(string propertyName)
     {
         PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
     }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
 }
 
 /// <summary>
@@ -35,13 +72,13 @@ public class FilterSettings : INotifyPropertyChanged
 /// </summary>
 public partial class VersionFilters : Window
 {
-    private DirectoryInfo VersionDir;
+    private DirectoryInfo _VersionDir;
 
     public static string FilterJson => ".Filter.json";
 
-    private string FilterJsonPath => Path.Combine(VersionDir.FullName, FilterJson);
+    private string _FilterJsonPath => Path.Combine(_VersionDir.FullName, FilterJson);
 
-    private FilterSettings? filterSettings { get; set; } = null;
+    private FilterSettings _FilterSettings { get; set; }
 
     /// <summary>
     /// Initialize the form for filtering a VS History directory.
@@ -51,44 +88,57 @@ public partial class VersionFilters : Window
     {
         InitializeComponent();
 
-        VersionDir = _directory;
+        _VersionDir = _directory;
+
+        //
+        // If there is a settings file, load it.
+        //
+        _FilterSettings = new(_FilterJsonPath);
+
+        //
+        // Set the DataContext to enable the Bindings.
+        //
+        DataContext = _FilterSettings;
+
+        EnableControls();
     }
 
     /// <summary>
-    /// Clear the first search string, which will trigger the TextChanged event.
+    /// Clear the search strings, which will trigger the TextChanged event.
     /// </summary>
     /// <param name="sender"></param>
     /// <param name="e"></param>
     private void btnClear_Click(object sender, RoutedEventArgs e)
     {
-        filterSettings.searchString1= string.Empty;
-        filterSettings.searchString2= string.Empty;
-    
+        _FilterSettings.searchString1 = string.Empty;
+        _FilterSettings.searchString2 = string.Empty;
+
         searchString2.Text = string.Empty;
         searchString1.Text = string.Empty;
     }
 
-
+    /// <summary>
+    /// If there are any filter settings (i.e. searchString1 isn't empty),
+    /// save the settings in the filter file.  If there aren't, delete
+    /// the filter file if it exists.
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
     private void btnOK_Click(object sender, RoutedEventArgs e)
     {
-        FileInfo fileInfo = new FileInfo(FilterJsonPath);
+        FileInfo fileInfo = new FileInfo(_FilterJsonPath);
         string sSearch = searchString1.Text;
 
         if (!string.IsNullOrWhiteSpace(sSearch))
         {
+            //
+            // There are some settings -- save them.
+            //
             try
             {
-                //
-                // Save the disk info in a JSON file.
-                //
-                JsonSerializerOptions jsonOptions = new JsonSerializerOptions()
-                {
-                    WriteIndented = true,
-                };
-
                 using (FileStream fsJson = fileInfo.OpenWrite())
                 {
-                    JsonSerializer.Serialize(fsJson, filterSettings, jsonOptions);
+                    JsonSerializer.Serialize(fsJson, _FilterSettings);
                 }
             }
             catch
@@ -120,11 +170,18 @@ public partial class VersionFilters : Window
         Close();
     }
 
+    /// <summary>
+    /// Enables or disables the second half of the form based on
+    /// the presence of text in the first search string input.
+    /// </summary>
+    /// <remarks>
+    /// If the <see cref="searchString1"/> input is empty or contains only whitespace,
+    /// the controls in the second part of the form are disabled.
+    /// Otherwise, they are enabled. This method ensures that the second
+    /// set of controls is only accessible when the first input is valid.
+    /// </remarks>
     private void EnableControls()
     {
-        //
-        // If there is no string in searchString1, disable the second half of the form.
-        //
         if (string.IsNullOrWhiteSpace(searchString1.Text))
         {
             stackPart2.IsEnabled = false;
@@ -137,6 +194,12 @@ public partial class VersionFilters : Window
         }
     }
 
+    /// <summary>
+    /// The first search string changed.  Update the controls
+    /// based on whether or not it is empty.
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
     private void searchString1_TextChanged(object sender, TextChangedEventArgs e)
     {
         EnableControls();
@@ -144,28 +207,5 @@ public partial class VersionFilters : Window
 
     private void Window_Loaded(object sender, RoutedEventArgs e)
     {
-        filterSettings = null;
-
-        try
-        {
-            FileInfo fileInfo = new FileInfo(FilterJsonPath);
-
-            if (fileInfo.Exists)
-            {
-                filterSettings = JsonSerializer.Deserialize<FilterSettings>(fileInfo.OpenRead());
-            }
-        }
-        catch
-        {
-        }
-
-        //
-        // If we didn't get the settings from the file, fall back to defaults.
-        //
-        filterSettings ??= new();
-
-        DataContext = filterSettings;
-
-        EnableControls();
     }
 }
