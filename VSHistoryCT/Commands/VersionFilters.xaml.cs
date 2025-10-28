7dad91364197e53d99125955d264808410da626f
Ken Cross <kjchome@comcast.net>
2025-10-28 12:27:51 -0400

    wip

--- VSHistoryCT/Commands/VersionFilters.xaml.cs
+++ VSHistoryCT/Commands/VersionFilters.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.Text.Json;
 using System.Windows;
 using System.Windows.Controls;
 
@@ -7,11 +8,6 @@ namespace VSHistory;
 
 public class FilterSettings : INotifyPropertyChanged
 {
-    public FilterSettings(string FilterJsonPath)
-    {
-
-    }
-
     public bool radExclude1 { get; set; } = false;
 
     public bool radExclude2 { get; set; } = false;
@@ -26,7 +22,7 @@ public class FilterSettings : INotifyPropertyChanged
 
     public string searchString2 { get; set; } = "There";
 
-    public event PropertyChangedEventHandler PropertyChanged;
+    public event PropertyChangedEventHandler? PropertyChanged;
 
     protected void OnPropertyChanged(string propertyName)
     {
@@ -45,7 +41,7 @@ public partial class VersionFilters : Window
 
     private string FilterJsonPath => Path.Combine(VersionDir.FullName, FilterJson);
 
-    private FilterSettings filterSettings;
+    private FilterSettings? filterSettings { get; set; } = null;
 
     /// <summary>
     /// Initialize the form for filtering a VS History directory.
@@ -56,8 +52,6 @@ public partial class VersionFilters : Window
         InitializeComponent();
 
         VersionDir = _directory;
-        filterSettings = new FilterSettings(FilterJsonPath);
-        this.DataContext = filterSettings;
     }
 
     /// <summary>
@@ -67,12 +61,63 @@ public partial class VersionFilters : Window
     /// <param name="e"></param>
     private void btnClear_Click(object sender, RoutedEventArgs e)
     {
-        searchString1.Text = string.Empty;
+        filterSettings.searchString1= string.Empty;
+        filterSettings.searchString2= string.Empty;
+    
         searchString2.Text = string.Empty;
+        searchString1.Text = string.Empty;
     }
+
 
     private void btnOK_Click(object sender, RoutedEventArgs e)
     {
+        FileInfo fileInfo = new FileInfo(FilterJsonPath);
+        string sSearch = searchString1.Text;
+
+        if (!string.IsNullOrWhiteSpace(sSearch))
+        {
+            try
+            {
+                //
+                // Save the disk info in a JSON file.
+                //
+                JsonSerializerOptions jsonOptions = new JsonSerializerOptions()
+                {
+                    WriteIndented = true,
+                };
+
+                using (FileStream fsJson = fileInfo.OpenWrite())
+                {
+                    JsonSerializer.Serialize(fsJson, filterSettings, jsonOptions);
+                }
+            }
+            catch
+            {
+                //
+                // Something went wrong?
+                //
+                sSearch = string.Empty;
+            }
+        }
+
+        if (string.IsNullOrWhiteSpace(sSearch))
+        {
+            try
+            {
+                //
+                // There are no filter settings -- delete the file.
+                //
+                if (fileInfo.Exists)
+                {
+                    fileInfo.Delete();
+                }
+            }
+            catch
+            {
+            }
+        }
+
+        Close();
     }
 
     private void EnableControls()
@@ -99,6 +144,28 @@ public partial class VersionFilters : Window
 
     private void Window_Loaded(object sender, RoutedEventArgs e)
     {
+        filterSettings = null;
+
+        try
+        {
+            FileInfo fileInfo = new FileInfo(FilterJsonPath);
+
+            if (fileInfo.Exists)
+            {
+                filterSettings = JsonSerializer.Deserialize<FilterSettings>(fileInfo.OpenRead());
+            }
+        }
+        catch
+        {
+        }
+
+        //
+        // If we didn't get the settings from the file, fall back to defaults.
+        //
+        filterSettings ??= new();
+
+        DataContext = filterSettings;
+
         EnableControls();
     }
 }
